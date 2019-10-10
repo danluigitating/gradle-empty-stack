1d8ea9f2be62a32050263125dcfcc2407bef6658
Dan Luigi Tating <danluigitating@hotmail.com>
2019-10-10 12:06:58 +0800

    added Mobile

---
+++ src/main/java/Mobile.java
@@ -0,0 +1,9 @@
+public class Mobile {
+    public String name;
+    public String color;
+    public String brand;
+
+    public void call(String message){
+        System.out.println("Message: " + message);
+    }
+}
